3d34d7639904b5e3e7dd13b0f4c14a3cfbb0df3e
RoshRoslin <RoshRoslin@gmail.com>
2021-05-08 14:37:51 -0700

    Push Master

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const menu = [
+  {
+    id: 1,
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
+    price: 15.99,
+    img: './images/item-1.jpeg',
+    desc:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum non voluptates voluptatem quasi rem. Mollitia, debitis eaque porro exercitationem nobis repellat expedita accusantium minima distinctio, est dolorum, ex quam consequuntur!',
+  },
+  {
+    id: 2,
+    title: 'diner double',
+    category: 'lunch',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. At et voluptatibus alias amet magni! Nesciunt reiciendis iusto laborum ab quos sint, vitae deleniti dolorem provident nemo temporibus ipsa maiores quod?',
+  },
+  {
+    id: 3,
+    title: 'godzilla milkshake',
+    category: 'shakes',
+    price: 6.99,
+    img: './images/item-3.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium soluta, qui fugiat sequi explicabo quaerat ex officiis maiores tempora blanditiis dolorum accusamus autem architecto quia laudantium id error, aperiam deserunt!',
+  },
+  {
+    id: 4,
+    title: 'country delight',
+    category: 'breakfast',
+    price: 20.99,
+    img: './images/item-4.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus reiciendis tenetur non odit asperiores accusantium eaque ullam expedita quos unde, illo similique nemo dolores natus consequuntur error? Dolorem, nulla quisquam.',
+  },
+  {
+    id: 5,
+    title: 'egg attack',
+    category: 'lunch',
+    price: 22.99,
+    img: './images/item-5.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, iure, quos quaerat est incidunt fugiat velit reiciendis non, magni doloribus accusantium quas id illo aliquid? At ex harum beatae eligendi!',
+  },
+  {
+    id: 6,
+    title: 'oreo dream',
+    category: 'shakes',
+    price: 18.99,
+    img: './images/item-6.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus in obcaecati amet pariatur maxime fugiat a dignissimos minus distinctio dolorum. Illum assumenda modi ut sapiente rerum accusantium consectetur fugit quas.',
+  },
+  {
+    id: 7,
+    title: 'bacon overflow',
+    category: 'breakfast',
+    price: 8.99,
+    img: './images/item-7.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae, cumque eveniet! Necessitatibus velit officia incidunt quaerat nisi nihil commodi, porro atque consectetur sunt cupiditate dolor in fugiat exercitationem possimus dolores.',
+  },
+  {
+    id: 8,
+    title: 'american classic',
+    category: 'lunch',
+    price: 12.99,
+    img: './images/item-8.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sint iure veritatis hic ipsum culpa distinctio tenetur sapiente! Nisi et voluptatem aut sed officiis officia exercitationem neque quae id pariatur! ',
+  },
+  {
+    id: 9,
+    title: 'quarantine buddy',
+    category: 'shakes',
+    price: 16.99,
+    img: './images/item-9.jpeg',
+    desc:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum at voluptas harum, voluptatum neque, in illo provident enim perferendis, sed ea obcaecati officia assumenda nobis quo maiores! Itaque, non iusto!',
+  },
+];
+export default menu;
