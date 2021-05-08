c8ba41aefe3c477850695a2676d1454f6a4ed1fd
RoshRoslin <RoshRoslin@gmail.com>
2021-05-08 14:37:24 -0700

    Updates

---
+++ precache-manifest.5172111c591895a5b3c23d398fa7712e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277467ac458e0f222f4a189290c116ef",
+    "url": "/react-menu-project/index.html"
+  },
+  {
+    "revision": "6210f1a26c1cf3ef821a",
+    "url": "/react-menu-project/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/react-menu-project/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-menu-project/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6210f1a26c1cf3ef821a",
+    "url": "/react-menu-project/static/js/main.cf1a7689.chunk.js"
+  },
+  {
+    "revision": "a7a2d7cb5a0efa71bab4",
+    "url": "/react-menu-project/static/js/runtime-main.6bf4de22.js"
+  }
+]);
